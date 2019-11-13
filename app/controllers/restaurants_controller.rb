32514b181ae2c8cfe67547a48a23ab0f74c883d4
Mohamed Ouasti <ouastim@gmail.com>
2019-11-13 16:27:03 +0100

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,7 +11,6 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
